393db82d2a2e46335196bf185b5ebc9e31a43d3e
Donny Bereznak <dbereznak@gmail.com>
2025-05-16 21:33:57 -0400

    Initial push

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    let name = "DBereznak.com";
+</script>
+
+<h1>Welcome to {name}</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
