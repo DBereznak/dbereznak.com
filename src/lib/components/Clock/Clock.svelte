7df7eb94f0aff39838479e1f30097efba93b4f86
Donny Bereznak <dbereznak@gmail.com>
2025-05-26 13:34:39 -0400

    updated footer. Added a clock

---
+++ src/lib/components/Clock/Clock.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+import './style.css';
+
+let getTime = () => {
+ const today = new Date;
+ return today.toLocaleTimeString();
+}
+let time = $state(getTime());
+
+$effect(() => {
+ const currentTime = setInterval(() => {
+    time = getTime();
+ }, 1000)
+});
+</script>
+
+<div class="clock">
+<h2> { time }</h2>
+</div>
--- src/lib/components/Footer/Footer.svelte
+++ src/lib/components/Footer/Footer.svelte
@@ -1,7 +1,18 @@
 <script>
 import "./Footer.css";
+import Clock from "../Clock/Clock.svelte";
+
+const year = $state( new Date);
 </script>
 
 <footer>
-    <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+    <div class="column">
+        <div>
+                <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+        </div>
+        <div class="copyright">
+          &copy; Donny Bereznak { year.getFullYear()}  
+        </div>
+        <Clock/>
+    </div>
 </footer>
