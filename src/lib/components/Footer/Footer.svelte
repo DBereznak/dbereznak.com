606ab2b387006d8f2060532a62364878f89a5d3e
Donny Bereznak <dbereznak@gmail.com>
2025-06-29 11:20:37 -0400

    started working on UI/UX footer looking good

--- src/lib/components/Footer/Footer.svelte
+++ src/lib/components/Footer/Footer.svelte
@@ -7,12 +7,15 @@ const year = $state( new Date);
 </script>
 
 <footer>
+  <a href="dbereznak@gmail.com">dbereznak@gmail.com</a>
+  <h1>Donny Bereznak<span>*</span></h1>
+  <hr/>
     <div class="column">
         <div>
                 <p><span class="text-adjust">Powered by </span> <a href="https://svelte.dev/docs/kit"><img style="width: 150px;" src={logo} alt="svelte logo" /></a></p>
         </div>
         <div class="copyright">
-          &copy; Donny Bereznak { year.getFullYear()}  
+          &copy; { year.getFullYear()} Donny Bereznak. All rights reserved 
         </div>
         <Clock/>
     </div>
