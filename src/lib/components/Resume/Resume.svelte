a535e4b64f3ac622d53a7b31a52f51f498c7e6a6
Donny Bereznak <dbereznak@gmail.com>
2025-07-02 21:58:45 -0400

    added more to resume section

--- src/lib/components/Resume/Resume.svelte
+++ src/lib/components/Resume/Resume.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 import './Resume.css';
+import  experience  from './experience';
 import resume from '$lib/images/Donny_resume.pdf';
 </script>
 
@@ -9,6 +10,22 @@ import resume from '$lib/images/Donny_resume.pdf';
         I am a passionate web developer with a flair for creating dynamic and engaging online experiences. With a strong foundation in both front-end and back-end technologies, I excel at transforming ideas into functional websites. My journey in web development has been marked by a commitment to continuous learning and innovation, ensuring that I stays ahead of industry trends. I believe in the power of collaboration and enjoys working with clients to bring their visions to life, crafting solutions that are not only visually appealing but also user-friendly.
     </p>
     <div class="content">
+        <h2>Past Experience</h2>
+        <div>
+            {#each experience as role}
+                <div class="role">
+                    <div class="duration">
+                    <p>{role.duration}</p>
+                    </div>
+                    <div class="role-details">
+                        <h3>{role.title}</h3>
+                        <h4>{role.company}</h4>
+                        <p>{role.description}</p>
+                    </div>
+                </div>
+                
+            {/each}
+        </div>
         <a href={resume} download class="download-button">Download Resume</a>
     </div>
 </section>
---
+++ src/lib/components/Resume/experience.ts
@@ -0,0 +1,46 @@
+const experience = [
+  {
+    title: "Software Engineer",
+    company: "Steampunk",
+    duration: "2023 - 2025",
+    description:
+      "Engineered MVP applications for the FNS Innovation Program, leveraging React and Python to rapidly deliver proof-of-concept solutions. Developed scalable, standards-compliant React components in TypeScript in alignment with WSWDS design guidelines. Built and maintained RESTful APIs using Flask and Node.js/Express to support pilot application functionality. Managed application state using React Hooks and Redux Toolkit for improved performance and maintainability. Additionally, implemented UiPath-based RPA workflows to demonstrate automation opportunities and streamline operations.",
+  },
+  {
+    title: "Frontend Web Developer",
+    company: "Perficient",
+    duration: "2021 - 2023",
+    description:
+      "Engineered modular web components and internal applications using React and PHP to support enterprise-grade CMS and training initiatives. Collaborated on the development of Meta’s Safety Center, delivering scalable solutions aligned with performance and usability standards. Continuously optimized CMS architecture by refining component logic and enhancing efficiency. Contributed effectively within cross-functional, remote teams to ensure timely delivery and seamless integration across projects.",
+  },
+  {
+    title: "Frontend Web Developer",
+    company: "Accenture",
+    duration: "2020 - 2021",
+    description:
+      "Enhanced the Veteran Affairs telehealth platform by developing and optimizing features with Angular 9, TypeScript, and .NET. Delivered interactive UI components—including an icon tray for reactions and hand-raising—to elevate user engagement during virtual sessions. Drove performance improvements by refining Angular architecture and implementing accessibility-focused UI enhancements. Collaborated seamlessly with cross-functional, remote teams to elevate overall platform functionality and user experience.",
+  },
+  {
+    title: "Frontend Web Developer",
+    company: "Stack Sports",
+    duration: "2016 - 2020",
+    description:
+      "Designed and implemented dynamic CMS templates using HTML, CSS, and C# to streamline content management workflows. Architected a custom e-commerce platform with Angular, TypeScript, and .NET, integrating RESTful APIs built in .NET Core for seamless data connectivity. Engineered bespoke UI components—such as a color picker and image slider—to enhance user experience, and upheld code quality standards through comprehensive unit testing with Jasmine.",
+  },
+  {
+    title: "Web Developer",
+    company: "Canongate",
+    duration: "2013 - 2016",
+    description:
+      "Designed, developed, and maintained responsive websites using HTML, CSS, JavaScript, and .NET, delivering optimized user experiences across devices. Created custom HTML email templates to support marketing initiatives and ensured visual consistency with brand guidelines. Proactively enhanced website performance and load times through front-end optimization techniques, while aligning web and email designs with broader branding strategies.",
+  },
+  {
+    title: "Web Developer",
+    company: "Air Services Inc",
+    duration: "2010 - Present",
+    description:
+      "Led the end-to-end development of a responsive WordPress website, focusing on user engagement and performance optimization. Designed and implemented a custom WordPress theme to enhance site speed and scalability, while applying SEO best practices that significantly improved search visibility and conversion rates. Managed website security and uptime to ensure a reliable, seamless user experience. Integrated third-party plugins and APIs to extend site functionality and operational efficiency",
+  },
+];
+
+export default experience;
