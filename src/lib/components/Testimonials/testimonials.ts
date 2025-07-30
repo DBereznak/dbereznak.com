f150a089a338b23e0f06f9573d145b49e68dbaba
Donny Bereznak <dbereznak@gmail.com>
2025-07-29 21:39:58 -0400

    updated some responsive css and edited a comment

--- src/lib/components/Testimonials/testimonials.ts
+++ src/lib/components/Testimonials/testimonials.ts
@@ -1,35 +1,35 @@
 const testimonials = [
-        {
-        name: "Linton Russell",
-        role: "Software Engineer",
-        company: "Steampunk",
-        testimonial: `I had the pleasure of managing Donny for a year and a half. He consistently delivered high-quality front-end work, translating complex designs into clean, responsive interfaces with great attention to detail.
+  {
+    name: "Linton Russell",
+    role: "Software Engineer",
+    company: "Steampunk",
+    testimonial: `I had the pleasure of managing Donny for a year and a half. He consistently delivered high-quality front-end work, translating complex designs into clean, responsive interfaces with great attention to detail.
                       Donny is a skilled developer with strong knowledge of modern frameworks, excellent problem-solving abilities, and a collaborative, professional attitude.`,
-    }, 
-    {
-        name: "Mia Rodriguez",
-        role: "Project Manager",
-        company: "Steampunk",
-        testimonial: `Donny was an invaluable front-end developer on the Innovation Hub team, known for his curiosity, teamwork, and hands-on approach. He took initiative where others hesitated. His "I'll figure it out" mindset made him a standout teammate. He engaged meaningfully in discussions, asked thoughtful questions, and truly invested in the success of both the product and the people around him. Any team would be fortunate to have someone as dependable and willing to learn as Donny.`,
-    },
-    {
-        name: "Sherrie Stringer",
-        role: "Office Manager",
-        company: "Air Services Inc",
-        testimonial: `The website designed for Air Services perfectly captures our 40+ years of expertise in aircraft maintenance and repair. It’s professional, easy to navigate, and has already helped us attract new clients while better serving our existing ones. We couldn’t be happier with the results—thank you for exceeding our expectations!`,
-    },
-    {
-        name: "Matt Simpson",
-        role: "UI/UX Tester",
-        company: "Steampunk",
-        testimonial: `I've had the pleasure of working with Donny from fall 2023 to fall 2024. Donny is a fantastic developer. Every time I've asked him questions, he eagerly responds quickly.`,
-    },
-        {
-        name: "Shannon Geraci",
-        role: "Director",
-        company: "Perficient",
-        testimonial: `During the time we worked together I observed that Donny took pride in his work and made sure the implementation matched the specs.`,
-    },
+  },
+  {
+    name: "Mia Rodriguez",
+    role: "Project Manager",
+    company: "Steampunk",
+    testimonial: `Donny was an invaluable front-end developer on the Innovation Hub team, known for his curiosity, teamwork, and hands-on approach. He took initiative where others hesitated. He truly invested in the success of both the product and the people around him. Any team would be fortunate to have someone as dependable and willing to learn as Donny.`,
+  },
+  {
+    name: "Sherrie Stringer",
+    role: "Office Manager",
+    company: "Air Services Inc",
+    testimonial: `The website designed for Air Services perfectly captures our 40+ years of expertise in aircraft maintenance and repair. It’s professional, easy to navigate, and has already helped us attract new clients while better serving our existing ones. We couldn’t be happier with the results—thank you for exceeding our expectations!`,
+  },
+  {
+    name: "Matt Simpson",
+    role: "UI/UX Tester",
+    company: "Steampunk",
+    testimonial: `I've had the pleasure of working with Donny from fall 2023 to fall 2024. Donny is a fantastic developer. Every time I've asked him questions, he eagerly responds quickly.`,
+  },
+  {
+    name: "Shannon Geraci",
+    role: "Director",
+    company: "Perficient",
+    testimonial: `During the time we worked together I observed that Donny took pride in his work and made sure the implementation matched the specs.`,
+  },
 ];
 
 export default testimonials;
